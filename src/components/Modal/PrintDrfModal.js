f56c737c00a78fd1609f483662606dc00f8f4ab4
Yisal Khan <yisal@krapplet.com>
2021-12-03 01:03:52 +0500

    complete the functionality of print drf

--- src/components/Modal/PrintDrfModal.js
+++ src/components/Modal/PrintDrfModal.js
@@ -22,6 +22,7 @@ import GridContainer from "../Grid/GridContainer.js";
 import Card from "../Card/Card.js";
 import CardHeader from "../Card/CardHeader.js";
 import Button from "../CustomButtons/Button.js";
+import AlertModal from "./AlertModal.js";
 
 import ChangeControlService from "../../Services/QA/Change_Control";
 import {
@@ -30,26 +31,16 @@ import {
 	RawMaterialSearchByName,
 	RawMaterialSearchByRMCode
 } from "../../Services/Inventory/inventory.js";
-import { ProductCodes } from "../../Services/Planning/A-Product_Selection.js";
 
-function createData(name, calories, fat, carbs, protein) {
-	return { name, calories, fat, carbs, protein };
-}
-
-const rows = [];
-
-export default function PrintDrfModal() {
+export default function PrintDrfModal(props) {
+	console.log(props);
 	const [open, setOpen] = React.useState(false);
 	const [fullWidth, setFullWidth] = React.useState(true);
 	const [maxWidth, setMaxWidth] = React.useState("md");
-	const [batchNo, setBatchNo] = useState("");
 	const [date, setDate] = useState();
-	const [productCodes, setProductCodes] = useState([]);
-	const [inputProductCodes, setInputProductCodes] = useState('');
-	const [selectedProductCode, setSelectedProductCode] = useState('');
 	const [highestDrf, setHighestDrf] = useState(0);
 	const [materialList, setMaterialList] = useState([]);
-	const [inputMaterial, setInputMaterial] = useState('');
+	const [inputMaterial, setInputMaterial] = useState("");
 	const [selectedMaterial, setSelectedMaterial] = useState("");
 	const [materialCodes, setMaterialCodes] = useState([]);
 	const [inputMaterialCode, setInputMaterialCode] = useState("");
@@ -58,18 +49,21 @@ export default function PrintDrfModal() {
 	const [formulaQuantity, setFormulaQuantity] = useState("");
 	const [additionalQuantity, setAdditionalQuantity] = useState("");
 	const [drfCart, setDrfCart] = useState([]);
+	const [showSuccess, setShowSuccess] = useState(false);
+	const [message, setMessage] = useState();
+	const [success, setSuccess] = useState();
 
 	useEffect(() => {
 		// if(highestDrf == 0) {
-			getHighestDRFNo();
+		getHighestDRFNo();
 		// }
 		getMaterialListAndCode();
-	}, [])
+	}, []);
 
 	const getHighestDRFNo = async () => {
 		const hdrf = (await ChangeControlService.methods.getHighestDRFNo()).data;
 		setHighestDrf(hdrf.DRFNo1);
-	}
+	};
 
 	const getMaterialListAndCode = async () => {
 		const resMaterialName = (await RawMaterialNames()).data;
@@ -77,81 +71,85 @@ export default function PrintDrfModal() {
 
 		const resMaterialCodes = (await RawMaterialCodes()).data;
 		setMaterialCodes(resMaterialCodes);
+	};
 
-		const productsCode = (await ProductCodes()).data;
-		setProductCodes(productsCode);
-	}
-
-	const getMaterialCodes = async (name) => {
-		if(name != "") {
+	const getMaterialCodes = async name => {
+		if (name != "") {
 			const resMaterialCodes = (await RawMaterialSearchByName(name)).data;
 			setInputMaterial(name);
 			setSelectedMaterial(name);
 			setInputMaterialCode(resMaterialCodes.RMCode);
 			setUnit(resMaterialCodes.Units);
 		}
-	}
+	};
 
-	const getMaterialName = async (code) => {
-		if(code != "") {
+	const getMaterialName = async code => {
+		if (code != "") {
 			const resMaterialName = (await RawMaterialSearchByRMCode(code)).data;
 			setInputMaterialCode(code);
 			setSelectedMaterialCode(code);
 			setInputMaterial(resMaterialName[0]?.Material);
 			setUnit(resMaterialName[0]?.Units);
 		}
-	}
+	};
 
 	const handleClickOpen = () => {
 		setOpen(true);
 	};
 
 	const handleClose = () => {
+		setDrfCart([]);
+		setDate();
+		setInputMaterial();
+		setInputMaterialCode();
+		setUnit();
+		setFormulaQuantity();
+		setAdditionalQuantity();
 		setOpen(false);
 	};
 
 	const handleAdd = async () => {
-		console.log('here', drfCart);
-		// debugger
-		let data = {
-			ProductCode: inputProductCodes,
-			BatchNo: batchNo,
-			Date: date,
-			demandedItems: [
-				{
-					RMCode: inputMaterialCode,
-					formulaQuantity: formulaQuantity,
-					additionalQuantity: additionalQuantity
-				}
-			]
-		};
-		if(drfCart && drfCart.length == 0) {
-			rows.push(data);
-			console.log(rows);
-			drfCart.push(data);
+		if (drfCart.demandedItems == undefined) {
+			let data = {
+				ProductCode: props.product,
+				BatchNo: props.batchNo,
+				Date: date,
+				demandedItems: [
+					{
+						RMCode: inputMaterialCode,
+						material: inputMaterial,
+						unit: unit,
+						formulaQuantity: formulaQuantity,
+						additionalQuantity: additionalQuantity,
+					},
+				],
+			};
+			setDrfCart(data);
 		} else {
-			console.log('in else');
-			const index = rows.findIndex(
-				x => x.ProductCode === data.ProductCode
-			);
-			if(index > -1) {
-				rows[index].demandedItems.push(data.demandedItems[0]);
-			} else {
-				rows.push(data);
-			}
-			console.log("row index", index);
-			console.log(rows);
-			// const index = drfCart.filter(x => x.ProductCode == data.ProductCode);
-			// console.log(index);
-			// drfCart[0].demandedItems.push(data.demandedItems[0]);
+			let data = {
+				RMCode: inputMaterialCode,
+				material: inputMaterial,
+				unit: unit,
+				formulaQuantity: formulaQuantity,
+				additionalQuantity: additionalQuantity,
+			};
+			drfCart.demandedItems.push(data);
 		}
-		// console.log(drfCart);
-		// drfCart.push(data);
-		// console.log(drfCart);
-		// const resp = await ChangeControlService.methods.postDRFView(data);
-		// console.log(resp);
+	};
 
-		// handleClose();
+	const handlePost = async () => {
+		const res = (await ChangeControlService.methods.postDRFView(drfCart)).data;
+		if (res.product != "" && res.batchNo != "") {
+			handleClose();
+			setShowSuccess(true);
+			setSuccess(true);
+			setMessage('Dispensation Request');
+		} else {
+			handleClose();
+			setShowSuccess(true);
+			setSuccess(false);
+			setMessage("Something went wrong.");
+		}
 	}
 
 	return (
@@ -162,6 +160,7 @@ export default function PrintDrfModal() {
 				fullWidth="true"
 				startIcon={<PrintIcon />}
 				onClick={handleClickOpen}
+				disabled={props.product == undefined || props.product == ""}
 			>
 				Print DRF/Copy
 			</Button>
@@ -203,36 +202,14 @@ export default function PrintDrfModal() {
 								</GridItem>
 
 								<GridItem xs={12} sm={12} md={3}>
-									<Autocomplete
-										disablePortal
-										id="drf-product"
-										value={selectedProductCode}
-										inputValue={inputProductCodes}
-										options={productCodes}
-										getOptionLabel={option => option.ProductCode}
-										onInputChange={(event, value) => {
-											if (value != "") {
-												setInputProductCodes(value);
-												setSelectedProductCode(value);
-											}
-										}}
-										renderInput={params => (
-											<TextField
-												{...params}
-												label="Product Code:"
-												variant="outlined"
-											/>
-										)}
-									/>
-									{/* <TextField
+									<TextField
 										id="drf-product"
 										label="Product:"
 										fullWidth="true"
+										value={props.product}
 										variant="outlined"
-										// onChange={event => {
-										// 	setProduct(event.target.value);
-										// }}
-									/> */}
+										InputProps={{ readOnly: true }}
+									/>
 								</GridItem>
 
 								<GridItem xs={12} sm={12} md={3}>
@@ -240,10 +217,9 @@ export default function PrintDrfModal() {
 										id="drf-batch-no"
 										label="Batch No:"
 										fullWidth="true"
+										value={props.batchNo}
 										variant="outlined"
-										onChange={event => {
-											setBatchNo(event.target.value);
-										}}
+										InputProps={{ readOnly: true }}
 									/>
 								</GridItem>
 							</GridContainer>
@@ -352,38 +328,46 @@ export default function PrintDrfModal() {
 					<Button variant="contained" color="primary" onClick={handleAdd}>
 						Add
 					</Button>
-					<Button variant="contained" color="primary" onClick={handleClose}>
+					<Button variant="contained" color="primary" onClick={handlePost}>
 						Post
 					</Button>
 					<Button variant="contained" color="primary" onClick={handleClose}>
-						Duplicate Copy
+						Cancel
 					</Button>
 				</DialogActions>
 				<TableContainer component={Paper}>
 					<Table aria-label="simple table">
 						<TableHead>
 							<TableRow>
-								<TableCell>Product Code</TableCell>
-								<TableCell align="right">RM Code</TableCell>
-								<TableCell align="right">Formula Quantity</TableCell>
-								<TableCell align="right">Additional Quantity</TableCell>
+								<TableCell>Sr.</TableCell>
+								<TableCell>Code</TableCell>
+								<TableCell>Material</TableCell>
+								<TableCell>Unit</TableCell>
 							</TableRow>
 						</TableHead>
 						<TableBody>
-							{rows.map(row => (
-								<TableRow key={row.ProductCode}>
-									<TableCell component="th" scope="row">
-										{row.ProductCode}
-									</TableCell>
-									<TableCell align="right">{row.calories}</TableCell>
-									<TableCell align="right">{row.fat}</TableCell>
-									<TableCell align="right">{row.carbs}</TableCell>
-								</TableRow>
-							))}
+							{drfCart.demandedItems != undefined &&
+								drfCart.demandedItems.map((row, index) => {
+									return (
+										<TableRow key={row.ProductCode}>
+											<TableCell component="th" scope="row">
+												{index + 1}
+											</TableCell>
+											<TableCell>{row.RMCode}</TableCell>
+											<TableCell>{row.material}</TableCell>
+											<TableCell>{row.unit}</TableCell>
+										</TableRow>
+									);
+								})}
 						</TableBody>
 					</Table>
 				</TableContainer>
 			</Dialog>
+			{showSuccess == true && (<AlertModal
+				showOpen={showSuccess}
+				message={message}
+				success={success}
+			/>)}
 		</div>
 	);
 }
