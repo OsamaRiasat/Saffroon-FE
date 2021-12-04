929ff7a1d8e53b9b579ffabbdbe337adb46177c4
Yisal Khan <yisal@krapplet.com>
2021-12-04 20:15:39 +0500

    update API structure and remove extra code

--- src/Services/QA/RM_Sample.js
+++ src/Services/QA/RM_Sample.js
@@ -4,42 +4,17 @@ import { apiUrl } from "../../config.json";
 const data = "QualityAssurance/";   // Its products intentially
 const Endpoint = apiUrl + data;
 
-export default {
-  methods: {
+export function GRNOList() {
+  const resp = http.get(Endpoint + "GRNOList/");
+  return resp;
+}
 
-    //List of GRNO's with with status "QUARANTINED"
-    GRNOList() {
-      console.log("Gettting GRNOList");
-      const resp = http.get(Endpoint + "GRNOList/");
-      console.log(resp);
-      return resp;
-    },
+export function RecievingDetailByGRNo(GRNo) {
+  const resp = http.get(Endpoint + "RMRecievingDetailByGRNo/" + GRNo + "/");
+  return resp;
+}
 
-    // When GRNo is selected
-    RecievingDetailByGRNo(GRNo) {
-        console.log("Gettting RMRecievingDetailByGRNo");
-        const resp = http.get(Endpoint + "RMRecievingDetailByGRNo/" + GRNo + "/");
-        console.log(resp);
-        return resp;
-    },
-    
- 
-    // {
-    //     "QCNo": "string",
-    //     "GRNo": 0,
-    //     "deliveredBy": "string",
-    //     "receivedBy": "string"
-    //   }
-
-    // Post Button
-    Sample(obj) {
-
-        console.log("Posting RMSample")
-        const resp = http.post(Endpoint + "RMSample/", obj);
-        console.log(resp);
-        return resp;
-    
-    },
-  },
-
-};
+export function Sample(obj) {
+  const resp = http.post(Endpoint + "RMSample/", obj);
+  return resp;
+}
