1e652181693157d738eae5060751056909c6781d
Yisal Khan <yisal@krapplet.com>
2021-12-04 19:57:20 +0500

    update API structure and remove extra code

--- src/Services/QA/Product_Sample.js
+++ src/Services/QA/Product_Sample.js
@@ -4,81 +4,27 @@ import { apiUrl } from "../../config.json";
 const data = "QualityAssurance/";   // Its products intentially
 const Endpoint = apiUrl + data;
 
-export default {
-  methods: {
+export function PSPCode() {
+const resp = http.get(Endpoint + "PSPCode/");
+return resp;
+}
 
-    //Response
-    // [
-    //     {
-    //       "ProductCode": "BS"
-    //     }
-    //   ]
-    //List of product codes
-    PSPCode() {
-      console.log("Gettting PSPCode");
-      const resp = http.get(Endpoint + "PSPCode/");
-      console.log(resp);
-      return resp;
-    },
+export function PSBatchNo(Pcode) {
+  const resp = http.get(Endpoint + "PSBatchNo/" + Pcode + "/");
+  return resp;
+}
 
-        //Response
-    // [
-    //     {
-    //       "batchNo": "BS02"
-    //     }
-    //   ]
-    // When ProductCode is selected
-    PSBatchNo(Pcode) {
-        console.log("Gettting PSBatchNo");
-        const resp = http.get(Endpoint + "PSBatchNo/" + Pcode + "/");
-        console.log(resp);
-        return resp;
-    },
+export function PSBatchDetail(batchNo) {
+  const resp = http.get(Endpoint + "PSBatchDetail/" + batchNo + "/");
+  return resp;
+}
 
-            //Response
-        // {
-        //     "MFGDate": "2021-09-01",
-        //     "EXPDate": "2021-09-01",
-        //     "currentStage": "Blistering",
-        //     "batchSize": 4000,
-        //     "QCNo": "FP21091921"
-        //   }
-    
-    // When Batch No is selected
-    PSBatchDetail(batchNo) {
-        console.log("Gettting PSBatchDetail");
-        const resp = http.get(Endpoint + "PSBatchDetail/" + batchNo + "/");
-        console.log(resp);
-        return resp;
-    },
+export function AllUsers() {
+  const resp = http.get(Endpoint + "AllUsers/");
+  return resp;
+}
 
-
-    //List of users 
-    AllUsers() {
-        console.log("Gettting AllUsers");
-        const resp = http.get(Endpoint + "AllUsers/");
-        console.log(resp);
-        return resp;
-      },
-    
-    
-    //   {
-    //     "batchNo": "string",
-    //     "sampleStage": "string",
-    //     "sampledBy": "string",
-    //     "sampleQuantity": "string",
-    //     "sampleUnity": "string"
-    //   }
-
-    // Post Button
-    ProductSample(obj) {
-
-        console.log("Posting ProductSample")
-        const resp = http.post(Endpoint + "ProductSample/", obj);
-        console.log(resp);
-        return resp;
-    
-    },
-  },
-
-};
+export function ProductSample(obj) {
+  const resp = http.post(Endpoint + "ProductSample/", obj);
+  return resp;
+}
