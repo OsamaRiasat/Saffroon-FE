f36b5d4ca93fc3e6afc3bcfe7ad10e87f5ce1508
OsamaRiasat <osamariasat@gmail.com>
2022-04-28 10:41:18 +0500

    ok

---
+++ src/Services/QA/Close_Batch.js
@@ -0,0 +1,19 @@
+import http from "../http/httpService.js";
+import { apiUrl } from "../../config.json";
+
+const data = "QualityAssurance/";   // Its production intentially
+const Endpoint = apiUrl + data;
+
+export function OpenBatches() {
+	console.log("Getting OpenBatches");
+	const resp = http.get(Endpoint + "OpenBatches/");
+	console.log(resp);
+	return resp;
+}
+
+export function CloseBPR(batchNo,obj) {
+	console.log("Putting CloseBPR");
+	const resp = http.put(Endpoint + "CloseBPR/"+batchNo+"/", obj);
+	console.log(resp);
+	return resp;
+}
